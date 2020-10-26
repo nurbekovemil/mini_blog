0da266f4c918893ff365d8b3259070d6a322c042
emil <nurbekovemil@mail.ru>
2020-10-26 23:47:56 +0600

    make it better

---
+++ client/build/precache-manifest.89b9db506abbbf4a21b67e252c976c0f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "265bae4df9664cc2976ab4b359e4b6b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0840e48f5b81a59dfbca",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "ed0e5090c83049c9d875",
+    "url": "/static/css/main.6710ecc5.chunk.css"
+  },
+  {
+    "revision": "0840e48f5b81a59dfbca",
+    "url": "/static/js/2.50723d5e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.50723d5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed0e5090c83049c9d875",
+    "url": "/static/js/main.e2206ed2.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
