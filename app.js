f38e915e728e37049a2d1eeff9a0174261816dcf
emil <nurbekovemil@mail.ru>
2020-10-26 21:26:56 +0600

    change package file

--- app.js
+++ app.js
@@ -15,10 +15,24 @@ app.use('/api/auth', require('./routes/auth.route'))
 app.use('/api/user', require('./routes/user.route'))
 app.use('/api/post', require('./routes/post.route'))
 
-app.use(express.static(__dirname, 'client'))
-app.use(express.static(path.resolve(__dirname, 'client', 'build')))
+// app.use(express.static(__dirname, 'client'))
+// app.use(express.static(path.resolve(__dirname, 'client', 'build')))
+// app.get('*', (req, res) => {
+// 	res.sendFile(path.resolve(__dirname,'client', 'build', 'index.html'))
+// })
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  //
+  app.get('*', (req, res) => {
+    res.sendfile(path.join(__dirname = 'client/build/index.html'));
+  })
+}
+//build mode
 app.get('*', (req, res) => {
-	res.sendFile(path.resolve(__dirname,'client', 'build', 'index.html'))
+  res.sendFile(path.join(__dirname+'/client/public/index.html'));
 })
 
 const MongoDB = async () => {
