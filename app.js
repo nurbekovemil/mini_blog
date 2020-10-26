014ab52417ab31c20efef31c4a8d4e161257df06
emil <nurbekovemil@mail.ru>
2020-10-27 00:04:15 +0600

    make it better

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express()
 app.use(express.json())
 
 app.use(cors())
-// app.use('/public', express.static('public'));
+app.use('/public', express.static('public'));
 
 app.use('/api/auth', require('./routes/auth.route'))
 app.use('/api/user', require('./routes/user.route'))
