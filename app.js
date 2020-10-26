5f2c5126bc73113352f9d96b627af59343e773f1
emil <nurbekovemil@mail.ru>
2020-10-26 22:03:20 +0600

    hhhhhhhhh

--- app.js
+++ app.js
@@ -25,14 +25,11 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 
 if(process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
-  //
-  app.get('*', (req, res) => {
-    res.sendfile(path.join(__dirname = 'client/build/index.html'));
-  })
+  
 }
 //build mode
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/public/index.html'));
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
 })
 
 const MongoDB = async () => {
