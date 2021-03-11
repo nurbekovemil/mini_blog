a358ec24225e48ebc36b64fb066a42d6c2452752
emil <nurbekovemil@mail.ru>
2021-03-11 20:25:13 +0600

    test commit

--- app.js
+++ app.js
@@ -23,12 +23,12 @@ app.use('/api/post', require('./routes/post.route'))
 // 	res.sendFile(path.join(__dirname,'client', 'build', 'index.html'))
 // })
 
-if(process.env.NODE_ENV === 'production') {
-  app.use('/', express.static(path.join(__dirname, 'client','build')));
-  app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'client','build','index.html'))
-	})
-}
+// if(process.env.NODE_ENV === 'production') {
+//   app.use('/', express.static(path.join(__dirname, 'client','build')));
+//   app.get('*', (req, res) => {
+// 		res.sendFile(path.resolve(__dirname, 'client','build','index.html'))
+// 	})
+// }
 //build mode
 
 
