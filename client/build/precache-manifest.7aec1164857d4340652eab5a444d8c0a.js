e1c9a1af90763a7b6ea04b7ecbca4b152a9ffe30
emil <nurbekovemil@mail.ru>
2021-03-11 20:10:38 +0600

    add build dicertory

---
+++ client/build/precache-manifest.7aec1164857d4340652eab5a444d8c0a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb931251d539db30f65c34b012d32f2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab65d83ccc16529c493f",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "c33c08a91e46c0715404",
+    "url": "/static/css/main.6710ecc5.chunk.css"
+  },
+  {
+    "revision": "ab65d83ccc16529c493f",
+    "url": "/static/js/2.5a380e50.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.5a380e50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c33c08a91e46c0715404",
+    "url": "/static/js/main.da601f87.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
