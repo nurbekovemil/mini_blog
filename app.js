ea841fd19148ee2895d7e454d537c8fe1a53da3e
emil <nurbekovemil@mail.ru>
2021-03-11 19:11:58 +0600

    run-up to heroku

--- app.js
+++ app.js
@@ -24,9 +24,9 @@ app.use('/api/post', require('./routes/post.route'))
 // })
 
 if(process.env.NODE_ENV === 'production') {
-  app.use('/', express.static(path.join(__dirname, 'client/build')));
+  app.use('/', express.static(path.join(__dirname, 'client','build')));
   app.get('*', (req, res) => {
-		res.sendFile(path.join(__dirname, 'client/build','index.html'));
+		res.sendFile(path.resolve(__dirname, 'client','build','index.html'))
 	})
 }
 //build mode
